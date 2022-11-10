5e63348923ca01c76d4543be95709d337c9b61bd
Daniel Torres <dmtm1404@gmail.com>
2022-11-09 20:09:04 -0500

    Gala Added and fix the texture of the goose

---
+++ Assets/Scripts/GalaController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalaController : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
