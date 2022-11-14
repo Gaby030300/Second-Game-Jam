8ae508c4de519a03fc79f198a0e62f7597d24858
Gabriela Bohorquez <gaby030300@hotmail.com>
2022-11-13 19:12:35 -0500

    Audio Effects
    Audio Effects

--- Assets/Scripts/HealthSystem/GooseMakeDmg.cs
+++ Assets/Scripts/HealthSystem/GooseMakeDmg.cs
@@ -6,10 +6,20 @@ public class GooseMakeDmg : MonoBehaviour
 {
     public float damage;
 
+    //Audio Effects
+    AudioSource audioSource;
+    public AudioClip hitEffect;
+
+    private void Start()
+    {        
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            audioSource.PlayOneShot(hitEffect, 0.3f);
             other.GetComponent<PlayerHealth>().TakeDamage(20.0f);
         }
     }
